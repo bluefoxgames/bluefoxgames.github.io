ef69020c5251526349c61b8dc029fd65f03bd6e9
SparkleFox <kesaidelta@gmail.com>
2022-08-06 20:06:14 -0300

    Added scroll-animation

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
+// function updateContentByWidth(width) {
+//     if (width < 900) {
+//         $('.presentation .left').hide();
+//         $('#vertical-line').hide();
+//         $('.presentation h2').attr('align', 'center')
+//     }
+// };
+
 window.onload = function() {
     $('content').show();
     $('#loading').fadeOut(500);
 };
+
+// updateContentByWidth(899);
+
+window.addEventListener("scroll",function(){
+    if(scrollY > 200){
+        $('.scroll-anim').fadeOut(500);
+    } else {
+        $('.scroll-anim').fadeIn(500);
+    };
+});
